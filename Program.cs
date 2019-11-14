6da92d5b0454a9d6df78041db26c5b8e55301679
Shyamraja <t7bhsh00@students.oamk.fi>
2019-11-14 04:53:04 +0200

    inheritance in OOP

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           CarSalesman steveTheSalesman = new CarSalesman("Steve", "Rogers");
+           Console.WriteLine(steveTheSalesman.FullName);
+           steveTheSalesman.Sell();
+        
+         
+           CarSalesman erikTheSalesdude = new CarSalesman("Erik","Erikson");
+           Console.WriteLine(erikTheSalesdude.FullName);
+           erikTheSalesdude.Sell();
+        
+        }
+
+    }
+}
